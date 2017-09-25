a80ab813595dd6eb5a25e07db627d94160777ebb
nicolasrcp123 <nicolas19992016@hotmail.com>
2017-09-24 22:22:47 -0500

    Add files via upload

---
+++ alumno.js
@@ -0,0 +1,11 @@
+// JavaScript source code
+var alumno = {
+    "nombre": "Juan",
+    "edad": 17,
+    "cursos": [ "Algoritmos","Base de Datos"
+    ],
+    "direccion": {
+        "tipo": "Calle"
+    }
+};
+
